64b48f3ec40b400e406a7804f05f45973273730f
Fabian Schwab <fabian.schwab@ibm.com>
2024-11-04 15:51:16 +0100

    basics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,8 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="prose m-auto max-w-md pt-6">
+	<h1>Running Calculator</h1>
+	<p>Calculate total distance for your daily running workout</p>
+	{@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	let pace = $state('00:05:00');
+	let duration = $state('00:30:00');
+
+	let distance: number = $derived.by(() => {
+		let [paceHours, paceMinutes, paceSeconds] = pace.split(':').map(Number);
+		let [durationHours, durationMinutes, durationSeconds] = duration.split(':').map(Number);
+
+		let paceTime = paceHours * 3600 + paceMinutes * 60 + paceSeconds;
+		let durationTime = durationHours * 3600 + durationMinutes * 60 + durationSeconds;
+
+		return durationTime / paceTime;
+	});
+
+	let enableWarmUp: boolean = $state(false);
+	let enableCoolDown: boolean = $state(false);
+</script>
+
+<div class="mb-16 flex flex-col">
+	<div class="form-control w-52">
+		<label class="label cursor-pointer">
+			<span class="label-text">Warm up</span>
+			<input type="checkbox" class="toggle toggle-error" bind:checked={enableWarmUp} />
+		</label>
+	</div>
+	<div class="form-control w-52">
+		<label class="label cursor-pointer">
+			<span class="label-text">Cool down</span>
+			<input type="checkbox" class="toggle toggle-info" bind:checked={enableCoolDown} />
+		</label>
+	</div>
+</div>
+
+<div class="card max-w-xs bg-base-100 shadow-xl">
+	<div class="card-body inline-flex w-full flex-col gap-4">
+		<div class="w-full text-center text-lg font-bold">Main Workout</div>
+		<label class="form-control w-full">
+			<div class="label">
+				<span class="label-text">Duration</span>
+			</div>
+			<input
+				step="1"
+				min="0"
+				bind:value={duration}
+				type="time"
+				class="input input-bordered w-full"
+			/>
+		</label>
+		<label class="form-control w-full">
+			<div class="label">
+				<span class="label-text">Pace (Time for 1 km)</span>
+			</div>
+			<input bind:value={pace} type="time" step="1" min="0" class="input input-bordered w-full" />
+		</label>
+		<div class="label-text ml-1 font-semibold">
+			Split distance {distance.toFixed(2)} km
+		</div>
+	</div>
+</div>
